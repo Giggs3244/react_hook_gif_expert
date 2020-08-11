ed80af33137b315387191b94e78f738a9e2d0efe
bbedoya <bbedoya@proteccion.com.co>
2020-08-11 18:35:47 -0500

    Se agrega el build en la carpeta docs

---
+++ docs/precache-manifest.1fb996d8fb4657cfb618039505088642.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8bb1f53feb23258f68b180d333c7f13",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe90b33bf0d3687a6ecd",
+    "url": "/static/css/main.43b6881f.chunk.css"
+  },
+  {
+    "revision": "f282757b0c05702f74d8",
+    "url": "/static/js/2.b1f59dfb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b1f59dfb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe90b33bf0d3687a6ecd",
+    "url": "/static/js/main.357b25c1.chunk.js"
+  },
+  {
+    "revision": "e5ee6826b74226f47af5",
+    "url": "/static/js/runtime-main.b577d66e.js"
+  }
+]);
